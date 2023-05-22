8cda034c68b12b890eca00384c45cab58c5cba86
vladislavBT <vladislavtulilov12345678@gmail.com>
2023-05-22 11:33:48 +0300

    JavaAdvancedSetsAndMaps

---
+++ src/JavaAdvancedSetsAndMaps/main.java
@@ -0,0 +1,10 @@
+package JavaAdvancedSetsAndMaps;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
